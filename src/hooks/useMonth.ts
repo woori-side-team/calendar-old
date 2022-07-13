1d9ff7f1edd3797b7b953558ba7d563377a83bd4
Avantgarde95 <mathematicianscott@gmail.com>
2022-07-13 15:36:53 +0900

    [feat] Calendar for each month
    * Display the days of the current month
      * Color: Black / Red / Blue
    * Display some days of the previous month & next month
      * Color: Gray

---
+++ src/hooks/useMonth.ts
@@ -0,0 +1,52 @@
+export interface Day {
+  year: number;
+  month: number; // 1 ~ 12.
+  monthDay: number;
+}
+
+export function useMonth(year: number, month: number) {
+  const thisMonthDayCount = new Date(year, month, 0).getDate();
+  const thisMonthFirstDay = new Date(year, month - 1, 1);
+  const prevMonthLastDay = new Date(year, month - 1, 0);
+  const prevMonthDayCount = prevMonthLastDay.getDate();
+  const nextMonthFirstDay = new Date(year, month + 1, 1);
+
+  const weeks: Array<Array<Day>> = [[]];
+  let pushCount = 0;
+
+  function pushDay(day: Day) {
+    const currentWeek = weeks[weeks.length - 1];
+
+    if (currentWeek.length < 7) {
+      currentWeek.push(day);
+    } else {
+      weeks.push([day]);
+    }
+
+    pushCount++;
+  }
+
+  for (let monthDay = prevMonthDayCount - thisMonthFirstDay.getDay() + 1; monthDay <= prevMonthDayCount; monthDay++) {
+    pushDay({
+      year: prevMonthLastDay.getFullYear(),
+      month: prevMonthLastDay.getMonth() + 1,
+      monthDay,
+    });
+  }
+
+  for (let monthDay = 1; monthDay <= thisMonthDayCount; monthDay++) {
+    pushDay({ year, month, monthDay });
+  }
+
+  const remainingDayCount = 7 * 6 - pushCount;
+
+  for (let monthDay = 1; monthDay <= remainingDayCount; monthDay++) {
+    pushDay({
+      year: nextMonthFirstDay.getFullYear(),
+      month: nextMonthFirstDay.getMonth() + 1,
+      monthDay,
+    });
+  }
+
+  return weeks;
+}
--- src/templates/month/MonthView.tsx
+++ src/templates/month/MonthView.tsx
@@ -1,17 +1,34 @@
 import React from "react";
 import styled from "@emotion/styled";
+import { useMonth } from "hooks/useMonth";
+import { css, Theme } from "@emotion/react";
 
-const weekDayNames = Array.from("일월화수목금토");
+const weekDayNames = ["일", "월", "화", "수", "목", "금", "토"];
 
-const MonthView = () => (
-  <Container>
-    <Row>
-      {weekDayNames.map(name => (
-        <NameCell key={name}>{name}</NameCell>
+const MonthView = () => {
+  const year = 2022;
+  const month = 7;
+  const weeks = useMonth(year, month);
+
+  return (
+    <Container>
+      <Row>
+        {weekDayNames.map(name => (
+          <NameCell key={name}>{name}</NameCell>
+        ))}
+      </Row>
+      {weeks.map((week, index) => (
+        <Row key={index}>
+          {week.map(day => (
+            <DayCell isCurrentMonth={day.month === month} key={day.monthDay}>
+              {day.monthDay}
+            </DayCell>
+          ))}
+        </Row>
       ))}
-    </Row>
-  </Container>
-);
+    </Container>
+  );
+};
 
 const Container = styled.div`
   width: 100%;
@@ -25,27 +42,41 @@ const Row = styled.div`
   width: 100%;
 `;
 
-const Cell = styled.div`
+const cellStyle = css`
   text-align: center;
 
   width: 100%;
   flex: 1;
+`;
 
+const createDisabledStyle = (theme: Theme) => css`
+  color: ${theme.scale.scale3};
+`;
+
+const createHighlightStyle = (theme: Theme) => css`
   &:first-of-type {
-    color: ${({ theme }) => theme.tint.red};
+    color: ${theme.tint.red};
   }
 
   &:last-of-type {
-    color: ${({ theme }) => theme.tint.blue};
+    color: ${theme.tint.blue};
   }
 `;
 
-const NameCell = styled(Cell)`
+const NameCell = styled.div`
+  ${cellStyle}
+
   line-height: 28px;
+
+  ${({ theme }) => createHighlightStyle(theme)}
 `;
 
-const DayCell = styled(Cell)`
+const DayCell = styled.div<{ isCurrentMonth: boolean }>`
+  ${cellStyle}
+
   height: 44px;
+
+  ${({ isCurrentMonth, theme }) => (isCurrentMonth ? createHighlightStyle(theme) : createDisabledStyle(theme))}
 `;
 
 export default MonthView;
