2d7bbc0a0b06305a507ae9d49b50b1d9646737fa
Avantgarde95 <mathematicianscott@gmail.com>
2022-08-30 10:16:39 +0900

    [feat] Add checklist and memo to WeekView
    * CheckItemGallery
    * MemoGallery

--- src/schedule/templates/WeekView.tsx
+++ src/schedule/templates/WeekView.tsx
@@ -3,10 +3,11 @@ import { css, Theme } from "@emotion/react";
 import styled from "@emotion/styled";
 
 import { ReactComponent as NextWeekIcon } from "schedule/assets/NextWeek.svg";
+import { ReactComponent as MemoIcon } from "schedule/assets/Memo.svg";
 
 const WeekView = () => (
   <Container>
-    <Week>
+    <DayGallery>
       <OtherWeek targetWeek="Prev">
         <NextWeekIcon />
       </OtherWeek>
@@ -19,7 +20,24 @@ const WeekView = () => (
       <OtherWeek targetWeek="Next">
         <NextWeekIcon />
       </OtherWeek>
-    </Week>
+    </DayGallery>
+    <SectionTitle>
+      <MemoIcon /> 금주의 체크리스트
+    </SectionTitle>
+    <CheckItemGallery>
+      <CheckItem>체크체크</CheckItem>
+      <CheckItem>체크체크</CheckItem>
+    </CheckItemGallery>
+    <SectionTitle>
+      <MemoIcon /> 꼭 잊지 말아야 할 메모
+    </SectionTitle>
+    <MemoGallery>
+      <MemoCard>메모메모</MemoCard>
+      <MemoCard>메모메모</MemoCard>
+      <MemoCard>메모메모</MemoCard>
+      <MemoCard>메모메모</MemoCard>
+      <MemoCard>메모메모</MemoCard>
+    </MemoGallery>
   </Container>
 );
 
@@ -35,17 +53,18 @@ const Container = styled.div`
   padding-top: 28px;
 `;
 
-const Week = styled.div`
+const DayGallery = styled.div`
   overflow-x: auto;
   box-sizing: border-box;
   display: flex;
   flex-direction: row;
 
   width: 100%;
-  padding: 0 16px 28px 16px;
+  margin-bottom: 20px;
+  padding: 0 16px 12px 16px;
 `;
 
-const cardStyle = (theme: Theme) => css`
+const createDayCardStyle = (theme: Theme) => css`
   box-sizing: border-box;
 
   height: 160px;
@@ -66,7 +85,7 @@ interface DayCardProps {
 }
 
 const DayCard = styled.div<DayCardProps>`
-  ${({ theme }) => cardStyle(theme)}
+  ${({ theme }) => createDayCardStyle(theme)}
 
   width: 124px;
 
@@ -79,7 +98,7 @@ interface OtherWeekProps {
 }
 
 const OtherWeek = styled.div<OtherWeekProps>`
-  ${({ theme }) => cardStyle(theme)}
+  ${({ theme }) => createDayCardStyle(theme)}
 
   display: flex;
   flex-direction: row;
@@ -98,6 +117,69 @@ const MonthDay = styled.span`
 const WeekDay = styled.span`
   margin-left: 2px;
   font-size: 16px;
+`;
+
+const SectionTitle = styled.div`
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+
+  width: calc(100% - 32px);
+  padding-left: 20px;
+  padding-bottom: 14px;
+  padding-right: 32px;
+  border-bottom: 0.7px solid ${({ theme }) => theme.gray.gray3};
+  font-size: 18px;
+  font-weight: 400;
+
+  & > svg {
+    margin-right: 4px;
+  }
+`;
+
+const CheckItemGallery = styled.div`
+  width: 100%;
+  margin-bottom: 14px;
+`;
+
+const CheckItem = styled.div`
+  box-sizing: border-box;
+
+  width: 100%;
+  padding: 10px 24px 10px 28px;
+`;
+
+const MemoGallery = styled.div`
+  overflow-x: auto;
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: row;
+
+  width: 100%;
+  margin-bottom: 80px;
+  padding: 12px 16px 12px 16px;
+`;
+
+const MemoCard = styled.div`
+  box-sizing: border-box;
+
+  width: 100px;
+  height: 120px;
+  padding: 8px 11px;
+  flex-shrink: 0;
+  font-size: 12px;
+  font-weight: 400;
+
+  border: 1px solid ${({ theme }) => theme.scale.scale2};
+  border-radius: 12px;
+  background-color: transparent;
+
+  &:not(:first-of-type) {
+    margin-left: 4px;
+  }
+
+  color: ${({ theme }) => theme.scale.scale8};
 `;
 
 export default WeekView;
